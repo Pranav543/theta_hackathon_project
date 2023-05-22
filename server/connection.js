f35b848d398ad50ee675da03fd3038aa21928215
Pranav Naik <skadoosh@Pranavs-Mac-mini.local>
2023-05-22 23:30:16 +0530

    created mongoose connection

---
+++ server/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+async function connect(){
+  await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjv1qkz.mongodb.net/`, () => {
+        console.log("Database is connected")
+      
+    })
+    
+}
+
+connect()
+
+
